cfcbfdfe1f952110d31927705f929274bb56c1e7
Khushal Neekhra <ng05031975@gmail.com>
2023-09-26 22:41:02 +0530

    implemented stack in cpp

--- Stack/Implementation_of_Stack/CPP/Stack.cpp
+++ Stack/Implementation_of_Stack/CPP/Stack.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    //will gonna start making it soon :)
+    stack<int> stackint;
+    stackint.push(48);
+    stackint.push(67);
+    stackint.push(238);
+    stackint.push(9);
+
+    while (!stackint.empty()) {
+    cout << stackint.top() <<endl;
+    stackint.pop();
+    }
+
+    stackint.push(53);
+    stackint.push(67);
+    stackint.push(213);
+    stackint.push(91);
+
+    while (!stackint.empty()) {
+    cout << stackint.top() <<endl;
+    stackint.pop();
+    }
+
     return 0;
 }
 
