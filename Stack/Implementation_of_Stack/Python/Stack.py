db570529a4b68ffe0b8c426d5850dc2ac60aeedb
Khushal Neekhra <ng05031975@gmail.com>
2023-09-26 22:50:30 +0530

    implemented stack in Python

---
+++ Stack/Implementation_of_Stack/Python/Stack.py
@@ -0,0 +1,15 @@
+stack = []
+
+stack.append(23)
+stack.append(43)
+stack.append(63)
+stack.append(83)
+
+print(stack)
+print("\n")
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print("\n")
