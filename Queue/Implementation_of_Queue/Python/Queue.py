307925e4e54a77eee9a6619decc44237005d8462
Khushal Neekhra <ng05031975@gmail.com>
2023-10-01 22:40:16 +0530

    Implemented Queue in Stack

---
+++ Queue/Implementation_of_Queue/Python/Queue.py
@@ -0,0 +1,16 @@
+Queue = []
+
+Queue.append('2')
+Queue.append('4')
+Queue.append('5')
+Queue.append('1')
+Queue.append('7')
+Queue.append('3')
+Queue.append('6')
+
+print(Queue.pop(0))
+print(Queue.pop(0))
+print(Queue.pop(0))
+print(Queue.pop(0))
+
+print("Now the elements remain in Queue are =" , Queue)
